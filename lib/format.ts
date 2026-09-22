9a27c37fc36798a4d3c75cf4b8be4b2563b56e64
dvdianaa <devidianaagustin@gmail.com>
2026-09-22 21:37:58 +0700

    Update format.ts

--- lib/format.ts
+++ lib/format.ts
@@ -1,4 +1,6 @@
 // lib/format.ts
 export function toTitleCase(s: string) {
-  return s.toLowerCase().replace(/\b\p{L}/gu, (c) => c.toUpperCase())
+  return s
+    .toLowerCase()
+    .replace(/(^|[\s,/(-])([a-z])/g, (_match, sep: string, letter: string) => sep + letter.toUpperCase())
 }
